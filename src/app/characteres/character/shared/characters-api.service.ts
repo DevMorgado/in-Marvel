fba125bc6717a4fed9013371245774c071ba0fbc
DevMorgado <paulaopdm@gmail.com>
2022-02-02 12:17:42 -0300

    Implementado campo para buscar characters

--- src/app/characteres/character/shared/characters-api.service.ts
+++ src/app/characteres/character/shared/characters-api.service.ts
@@ -10,16 +10,22 @@ export class CharactersApiService {
   PUBLIC_KEY = '56496b3c452a124e305da6183c1b6b6e';
   HASH = '90be452ec6f4adf8aad8ecf7a14492a2';
   TIMESTAMP ='1643653262097';
-  URL_API = 'https://gateway.marvel.com/v1/public/characters?limit=12&ts='+this.TIMESTAMP+'&apikey='+this.PUBLIC_KEY+'&hash='+this.HASH;
-
+  
   constructor(private http: HttpClient) { }
-
+  
   getAllCharacteres():Observable<any>{
-    return this.http.get(this.URL_API).pipe(
+    const allCharacters = 'https://gateway.marvel.com/v1/public/characters?limit=12&ts='+this.TIMESTAMP+'&apikey='+this.PUBLIC_KEY+'&hash='+this.HASH;
+    return this.http.get(allCharacters).pipe(
       map((res: any) => {
         return res.data.results;
       }),
       retry(5)
     );
+  }
+
+  getCharacterByName(characterName:string):Observable<any>
+  {
+    const characterByName = 'https://gateway.marvel.com:443/v1/public/characters?limit=12&nameStartsWith='+characterName+'&ts='+this.TIMESTAMP+'&apikey='+this.PUBLIC_KEY+'&hash='+this.HASH;
+    return this.http.get(characterByName);
   }
 }
--- src/app/characteres/characteres.component.ts
+++ src/app/characteres/characteres.component.ts
@@ -11,13 +11,35 @@ export class CharacteresComponent implements OnInit {
 
   constructor(private characterService: CharactersApiService) { }
   allCharacters: Observable<any> | undefined;
+  showSearchResult: boolean = false;
+  searchedCharacter:any=[];
+  characterName!: string;
 
   ngOnInit(): void {
-    this.getCharacters()
+    this.showSearchResult = false;
+    this.getCharacters();
+    
   }
 
   getCharacters(){
     this.allCharacters = this.characterService.getAllCharacteres();
+  }
+
+  findCharacter(event:any)
+  {
+     this.characterName = event.target.value;
+     if(this.characterName.length == 0){
+       this.ngOnInit()
+     }else{
+      this.characterService.getCharacterByName(this.characterName).subscribe((result)=>{
+        if(result.data.count>0){
+          this.showSearchResult = true;
+          this.searchedCharacter = result.data.results;
+        }else{
+          this.ngOnInit();
+        }
+      })
+     }
   }
 
 }
