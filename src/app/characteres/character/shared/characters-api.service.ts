153465d31b96ae8dd1274d2eac69b7537cdd7d68
DevMorgado <paulaopdm@gmail.com>
2022-02-01 21:48:10 -0300

    Melhorias na api service

--- src/app/characteres/character/shared/characters-api.service.ts
+++ src/app/characteres/character/shared/characters-api.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, retry } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,11 +10,16 @@ export class CharactersApiService {
   PUBLIC_KEY = '56496b3c452a124e305da6183c1b6b6e';
   HASH = '90be452ec6f4adf8aad8ecf7a14492a2';
   TIMESTAMP ='1643653262097';
-  URL_API = 'https:gateway.marvel.com/v1/public/characters?ts=1643653262097&apikey=56496b3c452a124e305da6183c1b6b6e&hash=90be452ec6f4adf8aad8ecf7a14492a2';
+  URL_API = 'https://gateway.marvel.com/v1/public/characters?limit=12&ts='+this.TIMESTAMP+'&apikey='+this.PUBLIC_KEY+'&hash='+this.HASH;
 
   constructor(private http: HttpClient) { }
 
   getAllCharacteres():Observable<any>{
-    return this.http.get<any>(this.URL_API).pipe(map((data:any )=> data.data.results))
+    return this.http.get(this.URL_API).pipe(
+      map((res: any) => {
+        return res.data.results;
+      }),
+      retry(5)
+    );
   }
 }
